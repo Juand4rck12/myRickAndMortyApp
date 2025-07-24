bd3f08781b0a33f69a492ffa61b22c6ee5679641
Juan_Diego <juan3312orrego@gmail.com>
2025-07-24 09:55:18 -0500

    Home y seccion de caracteres separada
    Se agrega la sección del home principal y funcionalidad separada por componente para el llamado a caracteres.

--- rick-morty/src/App.jsx
+++ rick-morty/src/App.jsx
@@ -1,6 +1,7 @@
 import imageRickMorty from './images/rick-morty.png'
 import './App.css'
 import { useState } from 'react';
+import Characters from './components/Characters';
 
 function App() {
 
@@ -12,15 +13,19 @@ function App() {
 		setCharacters(CHARACTER_API.results);
     };
 
-	console.log(characters);
-
     return (
 		<>
 		<div className="App">
 			<header className="App-header">
 				<h1 className="title">Rick & Morty</h1>
-				<img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
-				<button onClick={reqApi} className='btn-search'>Buscar personajes</button>
+				{characters ? (
+					<Characters characters={characters} />	
+				) : (
+					<>
+					<img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
+					<button onClick={reqApi} className='btn-search'>Buscar personajes</button>
+					</>
+				)}
 			</header>
 		</div>
 		</>
