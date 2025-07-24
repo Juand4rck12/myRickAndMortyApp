1dcac95359dab2e0beba7f9dd3968b6eb6f5c631
Juan_Diego <juan3312orrego@gmail.com>
2025-07-23 23:21:37 -0500

    Llamada a API y filtrado de caracteres

--- rick-morty/src/App.jsx
+++ rick-morty/src/App.jsx
@@ -1,22 +1,29 @@
 import imageRickMorty from './images/rick-morty.png'
 import './App.css'
+import { useState } from 'react';
 
 function App() {
 
-    const reqApi = () => {
-        console.log("Clicking")
+	const [ characters, setCharacters ] = useState(null);
+
+    const reqApi = async () => {
+        const API = await fetch("https://rickandmortyapi.com/api/character");
+		const CHARACTER_API = await API.json();
+		setCharacters(CHARACTER_API.results);
     };
 
-  return (
-    <>
-      <div className="App">
-        <header className="App-header">
-            <h1 className="title">Rick & Morty</h1>
-            <img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
-            <button onClick={reqApi} className='btn-search'>Buscar personajes</button>
-        </header>
-      </div>
-    </>
+	console.log(characters);
+
+    return (
+		<>
+		<div className="App">
+			<header className="App-header">
+				<h1 className="title">Rick & Morty</h1>
+				<img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
+				<button onClick={reqApi} className='btn-search'>Buscar personajes</button>
+			</header>
+		</div>
+		</>
   )
 }
 
